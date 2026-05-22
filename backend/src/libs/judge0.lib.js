6c37491c60693ffd2622b6807d384a0ec2809b6d
saptak dutta <duttasaptak79@gmail.com>
2026-05-22 19:38:09 +0530

    submitBatch function for gudge0

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 export const getJudge0LanguageId = (language)=>{
     const languageMap = {
         "PYTHON" : 71,
@@ -9,3 +10,11 @@ export const getJudge0LanguageId = (language)=>{
 }
 
 
+export const submitBatch = async(submissions)=>{
+   const {data} = await axios.post(`${process.env.JUDE0_API_URL}/submission/batch?base64_encoded=false`,{
+    submissions
+   })
+   console.log("submition Result",data)
+
+   return data
+}
