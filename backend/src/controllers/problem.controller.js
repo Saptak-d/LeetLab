58f326460bf7ea78a3511523cb11aac6fd705e54
saptak dutta <duttasaptak79@gmail.com>
2026-05-22 19:25:48 +0530

    creating the create-problenm controller and create a function for judge0 to verify language  id  called -- getJudge0LanguageId

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,2 +1,52 @@
-export const createProblem = async(req , res)=>{}
+import { db } from "../libs/db.js"
+import { getJudge0LanguageId } from "../libs/judge0.lib.js";
 
+
+
+export const createProblem = async(req , res)=>{
+//going to get all the data from the request body
+const {title,description,difficulty,tags,examples,constrains,testcases,codeSnippets,referenceSolutions} = req.body ;
+//going to check the user role once again
+  if(req.user.role != "Admin"){
+    return res.status(403).json({
+        error : "You are not allowed create the a problem"
+    })
+  }
+//loop through each and every  solution for different languages 
+
+ try {
+    for(const [language , solutionCode] of Object.entries(referenceSolutions)){
+        const languageId = getJudge0LanguageId(language);
+        if(!languageId){
+            return res.status(400).json({error : `The Language ${language} is not supported`})
+        }
+
+        const submissions = testcases.map(({input , output})=>({
+            source_code : solutionCode ,
+            language_id : languageId,
+            stdin : input,
+            expected_output : output
+
+        }))
+
+        const submissionResult = await submitBatch(submissions)
+    }
+
+    
+ } catch (error) {
+    
+ }
+// 
+//
+
+}
+
+export const getAllProblems = async(req,res)=>{}
+
+export const getProblemById = async(req,res)=>{}
+
+export const updateProblem = async(req,res)=>{}
+
+export const deleteProblem = async(req,res)=>{}
+
+export const getAllProblemsSolvedByUser = async(req,res)=>{} 
---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,11 @@
+export const getJudge0LanguageId = (language)=>{
+    const languageMap = {
+        "PYTHON" : 71,
+        "JAVA" : 62,
+        "JAVASCRIPT" : 63,
+    }
+
+    return languageMap[language.toUpperCase()]
+}
+
+
